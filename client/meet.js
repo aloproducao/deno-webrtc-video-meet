b3309f6c6b32a4dfcdfa5530bfa8d977a166e9c6
aloproducao <43450424+aloproducao@users.noreply.github.com>
2022-01-15 21:44:28 -0300

    Update meet.js

--- client/meet.js
+++ client/meet.js
@@ -35,21 +35,7 @@ if (redirectToHttps && !isHttps) {
 }
 const configuration = {
   "iceServers": [
-    {
-      "urls": "stun:stun.l.google.com:19302",
-    },
-    {
-      "urls": "stun:stun1.l.google.com:19302",
-    },
-    {
-      "urls": "stun:stun2.l.google.com:19302",
-    },
-    {
-      "urls": "stun:stun3.l.google.com:19302",
-    },
-    {
-      "urls": "stun:stun4.l.google.com:19302",
-    },
+  
 
     {
       url: 'turn:numb.viagenie.ca',
